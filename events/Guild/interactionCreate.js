53830bcf4cc959f2d62ead10709a7492c859ab3c
Necrro <Imhuman2506@gmail.com>
2022-11-25 14:38:08 +0700

    e

--- events/Guild/interactionCreate.js
+++ events/Guild/interactionCreate.js
@@ -5,63 +5,63 @@ const { QuickDB } = require("quick.db");
 const db = new QuickDB();
 
 module.exports = {
-  name: "interactionCreate"
+    name: "interactionCreate"
 };
 
 client.on('interactionCreate', async (interaction) => {
-  if (interaction.isChatInputCommand()) {
-    const command = client.slash_commands.get(interaction.commandName);
+    if (interaction.isChatInputCommand()) {
+        const command = client.slash_commands.get(interaction.commandName);
 
-    if (!command) return;
+        if (!command) return;
 
-    try {
-      command.run(client, interaction, config, db);
-    } catch (e) {
-      console.error(e)
+        try {
+            command.run(client, interaction, config, db);
+        } catch (e) {
+            console.error(e)
+        };
     };
-  };
 
-  if (interaction.isUserContextMenuCommand()) { // User:
-    const command = client.user_commands.get(interaction.commandName);
+    if (interaction.isUserContextMenuCommand()) { // User:
+        const command = client.user_commands.get(interaction.commandName);
 
-    if (!command) return;
+        if (!command) return;
 
-    try {
-      command.run(client, interaction, config, db);
-    } catch (e) {
-      console.error(e)
+        try {
+            command.run(client, interaction, config, db);
+        } catch (e) {
+            console.error(e)
+        };
     };
-  };
 
-  if (interaction.isMessageContextMenuCommand()) { // Message:
-    const command = client.message_commands.get(interaction.commandName);
+    if (interaction.isMessageContextMenuCommand()) { // Message:
+        const command = client.message_commands.get(interaction.commandName);
 
-    if (!command) return;
+        if (!command) return;
 
-    try {
-      command.run(client, interaction, config, db);
-    } catch (e) {
-      console.error(e)
+        try {
+            command.run(client, interaction, config, db);
+        } catch (e) {
+            console.error(e)
+        };
     };
-  };
 
-  if (interaction.isModalSubmit()) { // Modals:
-    const modal = client.modals.get(interaction.customId);
+    if (interaction.isModalSubmit()) { // Modals:
+        const modal = client.modals.get(interaction.customId);
 
-    if (!modal) return interaction.reply({
-      embeds: [
-        new EmbedBuilder()
-          .setDescription('Something went wrong... Probably the Modal ID is not defined in the modals handler.')
-          .setColor('Red')
-      ],
-      ephemeral: true
-    });
+        if (!modal) return interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                .setDescription('Something went wrong... Probably the Modal ID is not defined in the modals handler.')
+                .setColor('Red')
+            ],
+            ephemeral: true
+        });
 
-    try {
-      modal.run(client, interaction, config, db);
-    } catch (e) {
-      console.error(e)
-    };
-  }
+        try {
+            modal.run(client, interaction, config, db);
+        } catch (e) {
+            console.error(e)
+        };
+    }
 });
 
